70cfa1ca4e2341f58af7e1c58eed1913891e3b2a
Brice F <brice.favre@gmail.com>
2022-12-03 17:45:16 +0100

    fix #4 Get The First star for this issue

--- src/js/aoc.js
+++ src/js/aoc.js
@@ -24,5 +24,40 @@
  * Day 3
  */
 function getRucksackSumOfPriorities(data) {
-    return 0;
+    var lines = data.split("\n");
+    var sum = 0;
+    var codeA = 'A'.charCodeAt(0);
+    var codeZ = 'Z'.charCodeAt(0);
+
+    var codea = 'a'.charCodeAt(0);
+    var codez = 'z'.charCodeAt(0);
+
+    lines.forEach(rucksack => {
+        var currentCode = 0;
+        rucksack = rucksack.trim();
+        if (rucksack.length == 0) {
+            return;
+        }
+        var middle = Math.floor(rucksack.length / 2);
+
+        var compartment1 = rucksack.substr(0, middle).split('');
+        var compartment2 = rucksack.substr(middle).split('');
+
+        compartment1.forEach(item =>  {
+            if (currentCode != 0) {
+                return;
+            }
+            if (compartment2.includes(item)) {
+                var charcode = item.charCodeAt(0);
+                if (charcode >= codeA && charcode <= codeZ) {
+                    currentCode = 26 + charcode - codeA + 1;
+                } else if (charcode >= codea && charcode <= codez) {
+                    currentCode = charcode - codea + 1;
+                }
+            }
+        })
+        sum += currentCode;
+    });
+
+    return sum;
 }
