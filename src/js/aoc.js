9fed7eb36eeed56e46eab7ea29d4c5aa30eaaed7
Brice F <brice.favre@gmail.com>
2022-12-03 18:22:36 +0100

    Fix #6 I just resolve the problem

--- src/js/aoc.js
+++ src/js/aoc.js
@@ -26,11 +26,6 @@
 function getRucksackSumOfPriorities(data) {
     var lines = data.split("\n");
     var sum = 0;
-    var codeA = 'A'.charCodeAt(0);
-    var codeZ = 'Z'.charCodeAt(0);
-
-    var codea = 'a'.charCodeAt(0);
-    var codez = 'z'.charCodeAt(0);
 
     lines.forEach(rucksack => {
         var currentCode = 0;
@@ -48,16 +43,54 @@ function getRucksackSumOfPriorities(data) {
                 return;
             }
             if (compartment2.includes(item)) {
-                var charcode = item.charCodeAt(0);
-                if (charcode >= codeA && charcode <= codeZ) {
-                    currentCode = 26 + charcode - codeA + 1;
-                } else if (charcode >= codea && charcode <= codez) {
-                    currentCode = charcode - codea + 1;
-                }
+                currentCode = getCurrentCode(item);
             }
         })
         sum += currentCode;
     });
 
     return sum;
+}
+
+function getBadgeSum(input) {
+    var lines = input.split("\n");
+    var sum = 0;
+
+    for (i = 0 ; i < lines.length - 2; i+=3) {
+        var currentCode = 0;
+        var firstElf = lines[i].split('');
+        var secondElf = lines[i+1].split('');
+        var thirdElf = lines[i+2].split('');
+
+        firstElf.forEach(item =>  {
+            if (currentCode != 0) {
+                return;
+            }
+            if (secondElf.includes(item) && thirdElf.includes(item)) {
+                currentCode = getCurrentCode(item);
+            }
+        })
+        sum += currentCode;
+    }
+
+
+    return sum;
+}
+
+function getCurrentCode(item) {
+    var currentCode;
+    var charcode = item.charCodeAt(0);
+    var codeA = 'A'.charCodeAt(0);
+    var codeZ = 'Z'.charCodeAt(0);
+
+    var codea = 'a'.charCodeAt(0);
+    var codez = 'z'.charCodeAt(0);
+
+    if (charcode >= codeA && charcode <= codeZ) {
+        currentCode = 26 + charcode - codeA + 1;
+    } else if (charcode >= codea && charcode <= codez) {
+        currentCode = charcode - codea + 1;
+    }
+
+    return currentCode;
 }
