ac95a5d78f46db9246b5b46a1b898d2a2cbc139b
Brice F <brice.favre@gmail.com>
2022-12-03 11:16:15 +0100

    Commit the skeleton of my AoC project with a runner and a js test system

---
+++ src/js/aoc.js
@@ -0,0 +1,25 @@
+/**
+ * This contains every function of Advent of Code
+ *
+ *            *
+ *           /.\
+ *          /..'\
+ *          /'.'\
+ *         /.''.'\
+ *         /.'.'.\
+ *  "'""""/'.''.'.\""'"'"
+ *        ^^^[_]^^^
+ * MERRY == CHRISTMAS == PEOPLE
+ */
+
+/**
+ * Day 1
+ */
+
+/**
+ * Day 2
+ */
+
+/**
+ * Day 3
+ */
