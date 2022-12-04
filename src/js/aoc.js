c3bd41c28ee0947f43b6c2e94ac24d449ea8642e
Brice F <brice.favre@gmail.com>
2022-12-04 09:32:01 +0100

    Info #6 Add unit test and change  data

--- src/js/aoc.js
+++ src/js/aoc.js
@@ -61,3 +61,54 @@ function getRucksackSumOfPriorities(data) {
 
     return sum;
 }
+
+function getBadgeSum(input) {
+    var lines = input.split("\n");
+    var sum = 0;
+
+    for (i = 0 ; i < lines.length - 2; i+=3) {
+        var currentCode = 0;
+        var firstElf = lines[i].split('');
+        var secondElf = lines[i+1].split('');
+        var thirdElf = lines[i+2].split('');
+
+        firstElf.forEach(item =>  {
+            if (currentCode != 0) {
+                return;
+            }
+            if (secondElf.includes(item) && thirdElf.includes(item)) {
+                currentCode = getCurrentCode(item);
+            }
+        })
+        sum += currentCode;
+    }
+
+
+    return sum;
+}
+
+function getCurrentCode(item) {
+    var currentCode;
+    var charcode = item.charCodeAt(0);
+    var codeA = 'A'.charCodeAt(0);
+    var codeZ = 'Z'.charCodeAt(0);
+
+    var codea = 'a'.charCodeAt(0);
+    var codez = 'z'.charCodeAt(0);
+
+    if (charcode >= codeA && charcode <= codeZ) {
+        currentCode = 26 + charcode - codeA + 1;
+    } else if (charcode >= codea && charcode <= codez) {
+        currentCode = charcode - codea + 1;
+    }
+
+    return currentCode;
+}
+
+/**
+ * --- Day 4: Camp Cleanup ---
+ */
+
+function findNumberOfOverlap(input) {
+    return 0;
+}
